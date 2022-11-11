96bd7674c24c4c24141057fc51c11b0a5d98426c
TheChukwuma <egwuonwuchukwuma74@gmail.com>
2022-11-11 15:47:50 +0100

    created a new springboot project

---
+++ src/main/java/com/example/airlinemanagementsystem/AirLineManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.airlinemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirLineManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirLineManagementSystemApplication.class, args);
+    }
+
+}
