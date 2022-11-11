56112655dd0b68ca146339db80e80b614bf160ff
TheChukwuma <egwuonwuchukwuma74@gmail.com>
2022-11-11 17:28:14 +0100

    created user entity

--- src/main/java/com/airlinemanagementsystem/entity/User.java
+++ src/main/java/com/airlinemanagementsystem/entity/User.java
@@ -1,10 +1,33 @@
 package com.airlinemanagementsystem.entity;
 
-public class User {
+import lombok.*;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity(name = "users")
+public class User extends BaseClass{
+
+    @Column(nullable = false,columnDefinition = "VARCHAR(100)")
     private String firstName;
+
+    @Column(nullable = false,columnDefinition = "VARCHAR(100)")
     private String lastName;
+
+    @Column(unique = true, nullable = false,columnDefinition = "VARCHAR(100)")
     private String email;
+
+    @Column(nullable = false, columnDefinition = "VARCHAR(100)")
     private String password;
+
+    @Column(nullable = false,columnDefinition = "VARCHAR(100)")
     private String phoneNumber;
+
+    @Column(columnDefinition = "boolean default false")
+    private boolean isVerified;
 }
